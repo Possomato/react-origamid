1a2e104e6d02019ae2417ed754cec75726803f35
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-11-15 09:45:07 -0300

    add class vite

---
+++ 01-react-iniciantes/vite/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <a className="ativo" href="https://www.google.com/" title="site do google" target="_blank">
+      Google
+    </a>
+  )
+}
