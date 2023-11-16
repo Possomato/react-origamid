509f4d7b5d6555a08cf03c4e70e4d4f7a3c05485
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-11-16 14:11:16 -0300

    add class componentes

---
+++ 01-react-iniciantes/06-componentes/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header';
+import Footer from './footer';
+import Form from './form/Form';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ 01-react-iniciantes/06-componentes/src/header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <div>teste s</div>;
+};
+
+export default Header;
