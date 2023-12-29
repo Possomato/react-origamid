4ec0ad01cece384a231019674e3d0f0c9af6a775
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-12-29 11:13:52 -0300

    update usestate desafio

--- 02-react-hooks/01-usestate/src/topicos/Desafio.jsx
+++ 02-react-hooks/01-usestate/src/topicos/Desafio.jsx
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
-import Produto from './desafio/Produto';
+import React, { useState } from 'react'
+import Produto from './desafio/Produto'
+
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// Crie uma interface com 3 botões, um para cada produto.
+// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+// Mostre apenas um produto por vez
+// Mostre a mensagem carregando... enquanto o fetch é realizado
+
 
 export default function Desafio() {
-  const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
+  const [resposta, setResposta] = useState('')
 
-  async function handleClick(event) {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
-    );
-    const json = await response.json();
-    setDados(json);
-    setCarregando(false);
+  function handleProduto(event){
+    setResposta(<Produto produto={event.target.innerText} />)
   }
+
   return (
     <div>
-      <button onClick={handleClick}>notebook</button>
-      <button onClick={handleClick}>tablet</button>
-      <button onClick={handleClick}>smartphone</button>
-      {carregando && <p>carregando...</p>}
-      {!carregando && dados && <Produto dados={dados} />}
+      <button onClick={handleProduto}>smartphone</button>
+      <button onClick={handleProduto}>notebook</button>
+      <button onClick={handleProduto}>tablet</button>
+      <div>
+        {resposta}
+      </div>
     </div>
-  );
+  )
 }
--- 02-react-hooks/01-usestate/src/topicos/desafio/Produto.jsx
+++ 02-react-hooks/01-usestate/src/topicos/desafio/Produto.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Produto({ dados }) {
+export default function Produto({ produto }) {
+  const [jsonBody, setJsonBody] = useState(null);
+
+  // Função assíncrona para fazer a requisição
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`);
+      const data = await response.json();
+      setJsonBody(data);
+    } catch (error) {
+      console.error('Erro na requisição:', error);
+    }
+  };
+
+  // Chama a função para fazer a requisição quando o componente é renderizado
+  fetchData();
+
+  // Renderiza o componente quando jsonBody estiver disponível
   return (
     <div>
-      <h1>{dados.nome}</h1>
-      <p>{dados.preco}</p>
-      <img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
+      {jsonBody && (
+        <div>
+          <h1>{jsonBody.id}</h1>
+          <p>Preço: R${jsonBody.preco}</p>
+          <img src={jsonBody.fotos[0].src} alt={jsonBody.fotos[0].titulo} />
+        </div>
+      )}
     </div>
   );
 }
