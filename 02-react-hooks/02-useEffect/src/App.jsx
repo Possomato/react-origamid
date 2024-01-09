000396ba827c1f75b4727f079b2af4cedddc60f1
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2024-01-09 18:03:50 -0300

    att  useEffect

--- 02-react-hooks/02-useEffect/src/App.jsx
+++ 02-react-hooks/02-useEffect/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Produto from './Produto';
 
 export default function App() {
+  const [produto, setProduto] = React.useState(null);
+
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto');
+    if(produtoLocal !== null){
+      setProduto(produtoLocal)
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (produto !== null) {
+      window.localStorage.setItem('produto', produto);
+    }
+  }, [produto]);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
   return (
     <div>
-      teste
+      <h1>Preferência: {produto}</h1>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+      <Produto produto={produto} />
     </div>
-  )
+  );
 }
